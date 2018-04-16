58809c0035c0df2f082c0780234aac54cbb00592
Zak-Olyarnik <zwo24@drexel.edu>
2018-04-15 21:52:03 -0400

    Preliminary inventory work

---
+++ Assets/_SCRIPTS/Cuttable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cuttable : MonoBehaviour
+{
+
+    [SerializeField] private GameObject resultOfCut;
+
+    void OnMouseOver()
+    {
+        resultOfCut.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        resultOfCut.SetActive(false);
+    }
+
+}
