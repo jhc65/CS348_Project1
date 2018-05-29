09d8d045c80985e872ffaf0d4c13dd9e404043b3
Corwin Belser <cmb539@drexel.edu>
2018-05-29 12:42:57 -0400

    A glitchy, but working tooltip on the main menu

--- Assets/_SCRIPTS/SettingsToast.cs
+++ Assets/_SCRIPTS/SettingsToast.cs
@@ -8,28 +8,16 @@ public class SettingsToast : MonoBehaviour
     [SerializeField] private Text toast;
     [SerializeField] private RectTransform backing;
 
-    private void OnMouseOver()
-    {
-        Debug.Log("toast");
-        toast.gameObject.SetActive(true);
-
-    }
-
-    private void OnMouseExit()
-    {
-        toast.gameObject.SetActive(false);
-    }
-
     public void TurnOn(string s)
     {
         toast.text = s;
         Vector2 cursorPos = new Vector2(Input.mousePosition.x - 1920/2, Input.mousePosition.y - 1080/2);
         backing.localPosition = cursorPos;
-        toast.gameObject.SetActive(true);
+        this.gameObject.SetActive(true);
     }
 
     public void TurnOff()
     {
-        toast.gameObject.SetActive(false);
+        this.gameObject.SetActive(false);
     }
 }
